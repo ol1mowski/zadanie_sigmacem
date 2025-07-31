59b6245c0da0545e53a63ead2f16a5fa19856149
Ol1mowski <olimarki123@wp.pl>
2025-07-31 12:59:14 +0200

    chore: update build script and enhance type declarations for css and image modules

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,41 @@
 /// <reference types="vite/client" />
 
+declare module '*.module.css' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
 declare module '*.css' {
   const content: string;
   export default content;
 }
 
 declare module '*.svg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpeg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.gif' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.webp' {
   const content: string;
   export default content;
 }
